a72e8b8de6b5ec599712d0e6d4375a42f372bf05
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-11-22 15:48:03 +0200

    Add action to show all current user attended events

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,4 +4,8 @@ before_action :authenticate_user!
   def created_events
     @created_events = current_user.created_events.all
   end
+
+   def attended_events
+    @attended_events = current_user.attended_events.all
+   end
 end
