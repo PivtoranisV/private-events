4ce76f16e74a73a785da735dfbd6c20b2e64ca01
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-11-22 16:24:29 +0200

    Add method to attend event

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,10 +2,15 @@ class UserController < ApplicationController
 before_action :authenticate_user!
 
   def created_events
-    @created_events = current_user.created_events.all
+    @created_events = current_user.created_events
   end
 
    def attended_events
-    @attended_events = current_user.attended_events.all
+    @attended_events = current_user.attended_events
+   end
+
+   def attend
+     Event.find(params[:id]).attendees << current_user
+     redirect_to event_path(params[:id]), notice: "You are now attending this event."
    end
 end
