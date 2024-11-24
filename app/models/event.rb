bebc5ad47b32fdd280cd0c99845eff2ddd4f48cb
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-11-24 13:58:06 +0200

    Add validation for event creations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,9 @@ class Event < ApplicationRecord
   has_many :attendances, dependent: :destroy
   has_many :attendees, through: :attendances, source: :user
 
-  validates :title, :description, :date, :location, :category, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :description, presence: true
+  validates :date, presence: true, comparison: { greater_than_or_equal_to: Date.today, message: "cannot be in the past" }
+  validates :location, presence: true, length: { maximum: 200 }
+  validates :category, presence: true, inclusion: { in: [ "Art", "Music", "Education", "Other" ], message: "%{value} is not a valid category" }
 end
